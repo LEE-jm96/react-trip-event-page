822b690a9feecddeb5503c732bce6b3dca0e9a20
ljm5138 <minmin1243@naver.com>
2021-10-08 14:10:59 +0900

    docs: App.js 파일 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const style = {
+      backgroundColor: "blue",
+      padding: "16px",
+      color: "white",
+      fontSize: "36px",
+    };
+    return <div style={style}>코멘토 안녕하세요!</div>;
+  }
 }
 
 export default App;
