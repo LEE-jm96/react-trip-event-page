1d88e6e7437a1c8e51279baa58f66baeb243c131
ljm5138 <minmin1243@naver.com>
2021-10-08 14:34:10 +0900

    docs: App.js파일 수정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     const style = {
+      display: 'flex',
+      height: '50px',
+      width: '350px',
+      justifyContent: 'center',
+      alignItems: 'center',
       backgroundColor: 'blue',
       padding: '16px',
       color: 'white',
-      fontSize: '36px',
+      fontSize: '36px', 
     };
-    
+
     return <div style={style}>코멘토 안녕하세요!</div>;
   }
 }
