d5ae36c214fb616e1dc6f10c7c055c9b540f8000
ljm5138 <minmin1243@naver.com>
2021-10-08 14:17:12 +0900

    message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     const style = {
-      backgroundColor: "blue",
-      padding: "16px",
-      color: "white",
-      fontSize: "36px",
+      backgroundColor: 'blue',
+      padding: '16px',
+      color: 'white',
+      fontSize: '36px',
     };
+    
     return <div style={style}>코멘토 안녕하세요!</div>;
   }
 }
