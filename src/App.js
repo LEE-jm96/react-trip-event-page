a1a9fd6e7696a47d4719a21bb439ca91a8351708
ljm5138 <minmin1243@naver.com>
2021-10-24 12:32:17 +0900

    fix: 메인 페이지 기능 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import TripList from './components/TripList';
 
 const App = () => {
-  const [data, setData] = useState('');
-  useEffect(() => {
-    axios.get('https://8565c6b5-f051-4dfe-8d55-37738289754f.mock.pstmn.io/jeju')
-      .then(res => setData(res.data))
-  }, []);
-
-  return(
-    <div>
-      {data}
-    </div>
-  )
-} 
+  return <TripList />;
+};
 
 export default App;
