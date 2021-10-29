45459591ec89c768ef4abe9580da4d7418031419
ljm5138 <minmin1243@naver.com>
2021-10-29 23:29:00 +0900

    fix: add carousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import TripList from './components/TripList';
+import Button from './components/Button';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
 
 const App = () => {
-  return <TripList />;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return(
+    <Slider {...settings}>
+      <div>
+        <img src={process.env.PUBLIC_URL + "/jeju.jpg"} width="100%" height="700px"/>
+      </div>
+      <div>
+        <TripList />
+      </div>
+      <div>
+        <Button/>
+      </div>
+    </Slider>
+  ) 
 };
 
 export default App;
