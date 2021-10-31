a09f0a373d317843e9ecdc96d552629dee960b77
ljm5138 <minmin1243@naver.com>
2021-11-01 01:46:31 +0900

    fix: image 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Item = styled.div`
+    margin-top: -0.3rem;
+`;
 const Home = () => {
     return(
-        <div>
-            <img src={process.env.PUBLIC_URL + "/jeju.jpg"} width="100%" height="100%"/>
-        </div>
+        <><div>
+            <img src={process.env.PUBLIC_URL + "/jeju.jpg"} width="100%" height="100%" />
+        </div><Item>
+                <img src={process.env.PUBLIC_URL + "/jeju2.jpg"} width="100%" height="100%" />
+            </Item></>
     );
 };
 
