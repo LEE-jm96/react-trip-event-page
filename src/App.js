c62cf1c80eb8a60d750fd83382196a952ddc0cee
ljm5138 <minmin1243@naver.com>
2021-11-01 01:20:10 +0900

    refactory:  컴포넌트 호출코드 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+//import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TripList from './components/TripList';
 import ThemeList from './components/ThemeList';
@@ -7,9 +7,9 @@ import ThemeList from './components/ThemeList';
 const App = () => {
   return(
     <div>
-      <Route Path="/" component={Home} exact={true} />
-      <Route path="/locations" component={TripList} />
-      <Route path="/themes" component={ThemeList} />
+      <Home/>
+      <TripList/>
+      <ThemeList/>
     </div>
   ) 
 };
