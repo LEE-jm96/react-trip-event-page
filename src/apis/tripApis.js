b98ba50234d284a265d3afcf31f0536a9944670a
ljm5138 <minmin1243@naver.com>
2021-10-31 17:21:29 +0900

    fix: 함수명 변경

--- src/apis/tripApis.js
+++ src/apis/tripApis.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const getTrip = async () => {
+const getLoc = async () => {
     const response = await axios.get(
         `${process.env.REACT_APP_API_KEY}`
     );
     return response;
 }
 
-const getTrip2 = async () => {
+const getTheme = async () => {
     const response2 = await axios.get(
         `${process.env.REACT_APP_API_KEY2}`
     );
     return response2;
 }
 
-export {getTrip, getTrip2};
+export {getLoc, getTheme};
